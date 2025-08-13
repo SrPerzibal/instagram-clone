c96d43a0079869f7bf4ba449e1044114a0ebbffc
sirperziball <efrainvergara1503@gmail.com>
2025-08-12 23:41:50 -0500

    ajustando mas errores

--- src/pages/Sidebar.jsx
+++ src/pages/Sidebar.jsx
@@ -52,17 +52,17 @@ function Sidebar() {
 
             {/* Barra inferior para móvil */}
             <div className="md:hidden fixed bottom-0 left-0 w-full bg-white border-t border-gray-300 flex justify-around items-center h-13 z-50">
-                <Link to="/" className="flex items-center justify-center">
+                <Link to="/" className="flex flex-col items-center justify-center">
                     <img src={home} className="h-6 w-auto" alt="Inicio" />
                     Inicio
                 </Link>
 
-                <Link to="/crear" className="flex items-center justify-center">
+                <Link to="/crear" className="flex flex-col items-center justify-center ">
                     <img src={crear} className="h-6 w-auto" alt="Crear" />
                     Crear Publicacion
                 </Link>
 
-                <Link to="/historia" className="flex gap-4 items-center h-10 rounded hover:bg-gray-200 text-left font-semibold">
+                <Link to="/historia" className="flex flex-col gap-4 items-center h-10 rounded hover:bg-gray-200 text-left font-semibold">
                     <img className="h-6 w-auto" src={crear} alt="Crear Historia" />
                     Crear Historia
                 </Link>
